607e5e62570e98df331f98af6b4c39f522648d19
rezniki <igorreznik2001@gmail.com>
2025-08-10 19:55:25 +0300

    UserFilter / created filter

---
+++ src/components/UserFilter.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import type { User } from "../types/user";
+
+type UserFilterProps = {
+    users: User[];
+    selectedUserId: number | null;
+    onChange: (userId: number | null) => void;
+};
+
+const UserFilter: React.FC<UserFilterProps> = ({ users, selectedUserId, onChange }) => {
+    return (
+        <div className="mb-4">
+        <label className="block mb-2 font-semibold">Фильтр по пользователю:</label>
+        <select
+            value={selectedUserId ?? ""}
+            onChange={(e) => {
+            const value = e.target.value;
+            onChange(value ? Number(value) : null);
+            }}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
+        >
+            <option value="">Все пользователи</option>
+            {users.map((user) => (
+            <option key={user.id} value={user.id}>
+                {user.name}
+            </option>
+            ))}
+        </select>
+        </div>
+    );
+};
+
+export default UserFilter;
