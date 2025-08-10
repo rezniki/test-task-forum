633a88401c78b6c338ad5b18595fe7bb0198ab35
rezniki <igorreznik2001@gmail.com>
2025-08-10 18:30:26 +0300

    PostCard / created card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,36 +1,46 @@
 import React from "react";
 import type { Post } from "../types/post";
+import { Link } from "react-router-dom";
 
 interface Props {
     post: Post;
     onLike: (id: number) => void;
     onDislike: (id: number) => void;
     onFavorite: (id: number) => void;
+    onDelete: (id: number) => void;
 }
 
-const PostCard: React.FC<Props> = ({ post, onLike, onDislike, onFavorite }) => {
+const PostCard: React.FC<Props> = ({ post, onLike, onDislike, onFavorite, onDelete }) => {
     return (
         <div className="border rounded p-4 shadow bg-white">
-        <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+        <Link to={`/post/${post.id}`}>
+            <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+        </Link>
         <p className="text-gray-700 mb-4">{post.body}</p>
         <div className="flex gap-3">
             <button
             className={`px-3 py-1 rounded ${post.liked ? "bg-green-500 text-white" : "bg-gray-200"}`}
             onClick={() => onLike(post.id)}
             >
-            👍 Like
+            👍
             </button>
             <button
             className={`px-3 py-1 rounded ${post.disliked ? "bg-red-500 text-white" : "bg-gray-200"}`}
             onClick={() => onDislike(post.id)}
             >
-            👎 Dislike
+            👎
             </button>
             <button
             className={`px-3 py-1 rounded ${post.favorite ? "bg-yellow-500 text-white" : "bg-gray-200"}`}
             onClick={() => onFavorite(post.id)}
             >
-            ⭐ Favorite
+            ⭐
+            </button>
+            <button
+            className="px-3 py-1 rounded bg-gray-300"
+            onClick={() => onDelete(post.id)}
+            >
+            🗑
             </button>
         </div>
         </div>
@@ -38,4 +48,3 @@ const PostCard: React.FC<Props> = ({ post, onLike, onDislike, onFavorite }) => {
 };
 
 export default PostCard;
-
