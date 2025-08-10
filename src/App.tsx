43e2dd5b4074f87b469741a935c01ee856ed2915
rezniki <igorreznik2001@gmail.com>
2025-08-10 22:33:54 +0300

    App, main / fix bags

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import PostsPage from "./pages/PostPage";
 import PostDetails from "./pages/PostDetails";
+import Profile from "./pages/Profile";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PostsPage />} />
-        <Route path="/posts/:id" element={<PostDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<PostsPage />} />
+      <Route path="/posts/:id" element={<PostDetails />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
-}
+};
 
 export default App;
+
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
